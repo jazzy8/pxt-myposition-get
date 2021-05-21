523b71cc539d5e2531d641eab57ea85ad9f28acf
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-05-21 13:45:24 -0700

    Kind of works, haven't tested it

--- main.ts
+++ main.ts
@@ -1,5 +1,34 @@
+function is_on_left_side () {
+    return bstrength > cstrength
+}
+function is_at_back () {
+    return astrength < cstrength && astrength < bstrength
+}
+function Scroll () {
+	
+}
 function is_in_center () {
     return astrength == bstrength && bstrength == cstrength
+}
+function get_location () {
+    if (is_in_center()) {
+        return "In The Road"
+    } else if (is_on_right_side() && is_at_front()) {
+        return "Base"
+    } else if (astrength == -110 && (bstrength == -66 && cstrength == -20)) {
+        return "Certain Place"
+    } else {
+        return "Unknown"
+    }
+}
+function is_on_right_side () {
+    return bstrength < cstrength
+}
+function is_centered_sideways () {
+    return bstrength == cstrength
+}
+function is_at_front () {
+    return astrength > cstrength
 }
 radio.onReceivedValue(function (name, value) {
     if (name == "a") {
@@ -19,3 +48,7 @@ radio.setGroup(3)
 astrength = 0
 bstrength = 0
 cstrength = 0
+let perfoot = 2.6744186046511627
+let atob = 230
+let btoc = 230
+let atoc = 230
