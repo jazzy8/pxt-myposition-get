6f97f97df61d9eae545d2ba63b7fc043d55671d7
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-05-21 13:21:27 -0700

    Added finding strengths, and if you are in the center.

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+function is_in_center () {
+    return astrength == bstrength && bstrength == cstrength
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "a") {
+        astrength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    }
+    if (name == "b") {
+        bstrength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    }
+    if (name == "c") {
+        cstrength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
+    }
+})
+let cstrength = 0
+let bstrength = 0
+let astrength = 0
+radio.setGroup(3)
+astrength = 0
+bstrength = 0
+cstrength = 0
